ee9aa79ab816f34cae76dbf980a3a77eebc524a7
Roshan <roshanbhuvad15@gmail.com>
2020-08-06 18:43:14 +0530

    fetched realtime user interaction

--- server.js
+++ server.js
@@ -36,6 +36,12 @@ io.on("connection", (socket) => {
         "message",
         formatMessage(botName, `${user.username} has joined the chat`)
       ); //this emit send message(broadcast) to all existing users except the connecting user
+
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
   });
   //console.log("New WS connection ...");
 
@@ -55,6 +61,12 @@ io.on("connection", (socket) => {
         "message",
         formatMessage(botName, `${user.username} has left the chat`)
       );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
     }
   });
 });
